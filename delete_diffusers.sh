22c9d59041b2cdbddfa74562e6f906c9c50a021f
Okuda <kyokuheki@gmail.com>
2019-06-28 20:55:22 +0900

    Update delete_diffusers.sh

--- delete_diffusers.sh
+++ delete_diffusers.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 j="`docker run -i -v/var/lib/coreos-install/user_data:/config.yml:ro  -v/etc/passwd:/passwd:ro kyokuheki/diffusers`"
+echo "$j"
 #echo "$j" | jq
 #invalid=(`echo "$j" | jq -r '.invalid[]'`)
-jq <<<"$j"
+#jq <<<"$j"
 invalid=(`jq -r '.invalid[]' <<<"$j"`)
+echo invalid: $invalid
 
 for u in ${invalid[@]}
 do
